3ed8a06dc25f5b353a1f604247a2b44ccf1819a4
Faizan Anwer Ali Rupani <faizananwer2011@gmail.com>
2023-01-29 17:58:31 +0500

    User Module, Controller, and Service added.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -8,6 +9,7 @@ import { AuthModule } from './auth/auth.module';
       isGlobal: true,
     }),
     AuthModule,
+    UserModule,
   ],
   providers: [],
 })
