3557c171d24fb9739ce0b0a25c33b2d59122b92d
Faizan Anwer Ali Rupani <faizananwer2011@gmail.com>
2023-01-29 18:26:41 +0500

    Auth file updated, User Decorator added.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { BasicStrategy } from './strategies/basic.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
+  imports: [JwtModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy, BasicStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  // private logger = new Logger(AuthService.name);
+
+  constructor(
+    private readonly jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
+
+  async login(payload: { user: any }) {
+    const { user } = payload;
+    return this.sendToken({ id: user.id });
+  }
+
+  async sendToken(data: { id: string }) {
+    const { accessToken } = await this.accessToken(data);
+    return { state: 'LOGIN', access: accessToken };
+  }
+
+  IsAccessVerified(payload) {
+    return true;
+  }
+
+  async accessToken(payload: { id: string }) {
+    return {
+      accessToken: this.jwtService.sign(
+        { id: payload.id },
+        {
+          secret: this.configService.get<string>('JWT_SECRET'),
+          expiresIn: this.configService.get<string>('JWT_EXPIRE'),
+        },
+      ),
+    };
+  }
+}
