24257174995bcb4e0c448c34c55d74cfcc47150d
Faizan Anwer Ali Rupani <faizananwer2011@gmail.com>
2023-01-29 17:59:41 +0500

    Film Module, Controller, and Service added.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { FilmModule } from './film/film.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { UserModule } from './user/user.module';
     }),
     AuthModule,
     UserModule,
+    FilmModule,
   ],
   providers: [],
 })
