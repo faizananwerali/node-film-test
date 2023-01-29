44b4e3b67096b393a125ccf41f36dd571cfbea0e
Faizan Anwer Ali Rupani <faizananwer2011@gmail.com>
2023-01-29 18:27:02 +0500

    Auth file updated, User Decorator added.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { ApiBasicAuth, ApiTags } from '@nestjs/swagger';
+import { User } from '@common/decorators';
+import { BasicAuthGuard } from '@common/guards';
 
+@ApiTags('Auth')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @ApiBasicAuth()
+  @UseGuards(BasicAuthGuard)
+  @Get('login')
+  async login(@User() user) {
+    return this.authService.login({ user });
+  }
+}
